be2f959eb6ce774a4c32630af3b2383f650a1b56
Object-ions <moshikoatia@gmail.com>
2024-02-29 22:53:39 -0800

    Add and config Tailwind and DaisyUI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-xl">Hello world</h1>
+      <button className="btn">Click</button>
+    </div>
+  );
 }
 
 export default App;
